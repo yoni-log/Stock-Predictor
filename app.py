f812030a2ec5234889a9778aa02f82e518f52bdd
ioanni <ioannisgiannopoulosloyola@gmail.com>
2024-10-27 17:32:39 -0500

    Updated date input

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ ctk.set_default_color_theme("blue")
 root = ctk.CTk()
 root.iconbitmap("shiba.ico")
 root.title("ShibaTrader")
-root.geometry("1280x720")
+root.geometry("860x720")
 
 # Frame for main content
 frame = ctk.CTkFrame(root)
@@ -24,35 +24,22 @@ stock_label.grid(row=0, column=0, sticky='w', padx=5, pady=5)
 preselected_stock = ctk.CTkComboBox(frame, values=preselected_stocks, width=100)
 preselected_stock.grid(row=0, column=1, padx=5, pady=5, sticky='w')
 
-# Display start date label and date entry calendar
-lower_date_label = ctk.CTkLabel(frame, text="Start Date")
-lower_date_label.grid(row=1, column=0, sticky='w')
-lower_date = DateEntry(frame, date_pattern="yyyy-mm-dd")
-lower_date.grid(row=1, column=1, padx=5, pady=5, sticky='w')
-
-# Display end date label and date entry calendar
-upper_date_label = ctk.CTkLabel(frame, text="End Date")
-upper_date_label.grid(row=2, column=0, sticky='w')
-upper_date = DateEntry(frame, date_pattern="yyyy-mm-dd")
-upper_date.grid(row=2, column=1, padx=5, pady=5, sticky='w')
+period_label = ctk.CTkLabel(frame, text="Period")
+period_label.grid(row=1, column=0, sticky='w')
+period = ctk.CTkComboBox(frame, values=["1d", "5d", "1mo", "1yr", "5yr", "max"], width=100)
+period.grid(row=1, column=1, padx=5, pady=5, sticky='w')
 
 # Display model label & dropbox
 model_label = ctk.CTkLabel(frame, text="Model")
-model_label.grid(row=3, column=0, sticky='w')
+model_label.grid(row=2, column=0, sticky='w')
 model_combobox = ctk.CTkComboBox(frame, values=["Linear Regression", "LSTM", "Random Forest", "Gradient Boosting"], width=100)
-model_combobox.grid(row=3, column=1, padx=5, pady=5, sticky='w')
+model_combobox.grid(row=2, column=1, padx=5, pady=5, sticky='w')
 
 # Display interval label & dropbox
 interval_label = ctk.CTkLabel(frame, text="Interval")
-interval_label.grid(row=4, column=0, sticky='w')
+interval_label.grid(row=3, column=0, sticky='w')
 interval_combobox = ctk.CTkComboBox(frame, values=["1m", "5m", "1h", "1d"], width=100)
-interval_combobox.grid(row=4, column=1, padx=5, pady=5, sticky='w')
-
-# Display disclaimer
-interval_desc_text = ("*DISCLAIMER* yfinance has limitations in keeping data at intervals below 1 day\n"
-                      "1h within past 730 days; 5m within past 60 days; 1m within past 7 days")
-interval_desc = ctk.CTkLabel(frame, text=interval_desc_text)
-interval_desc.grid(row=5, column=0, columnspan=3, sticky='w')
+interval_combobox.grid(row=3, column=1, padx=5, pady=5, sticky='w')
 
 # Show summary box
 summary_label = ctk.CTkLabel(frame, text="Summary")
@@ -77,7 +64,7 @@ plot_frame.pack(anchor='w', pady=10, padx=10, fill=ctk.BOTH, expand=True)
 # Trains the model with the given data and displays data through unfilled labels and summary box
 submit_button = ctk.CTkButton(root, text="Submit",
                               command=lambda: submit(summary_text, verdict_label, accuracy_label, mse_label,
-                                                     preselected_stock, lower_date, upper_date, model_combobox,
+                                                     preselected_stock, period, model_combobox,
                                                      plot_frame, interval_combobox))
 submit_button.pack(anchor='w', pady=10, padx=10)
 
